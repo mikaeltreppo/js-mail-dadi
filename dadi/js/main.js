5850e211fa8061d5695ba7122edfa0607fcbbf5f
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-27 17:28:51 +0100

    tentavtivo implementaione conta punti

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,16 +1,55 @@
 /*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+
+
+//dado user
+let diceDomUser = document.getElementById("userNum");
+
 let userNumber = Math.floor(Math.random() * 6 + 1);
-console.log(userNumber);
+
+diceDomUser.innerHTML = userNumber;
+// dado pc
+let diceDompc = document.getElementById("pcNum");
 
 let pcNumber = Math.floor(Math.random() * 6 + 1);
-console.log(pcNumber);
+
+diceDompc.innerHTML = pcNumber;
+//risultato
+let resultDom = document.getElementById("result");
+
+
 
 if (userNumber < pcNumber) {
-    console.log("Il computer vince")}
-    else if (userNumber == pcNumber) {
-        console.log("pareggio, riprova!")
-    }
-    else{
-        console.log("hai vinto!")
-    }
+    resultDom.innerHTML = " Il Pc ha vinto!"
+}
+else if (userNumber == pcNumber) {
+    resultDom.innerHTML = " Pareggio"
+}
+else {
+    resultDom.innerHTML = " Ehi!!! Hai vinto!"
+}
+
+
+//aggiunta contatore vincite
+let userWin = 0;
+let pcWin = 0;
+let gameNumber = 0;
+
+if (userNumber < pcNumber) {
+    pcWin++;
+    gameNumber;
+
+}
+else if (userNumber > pcNumber) {
+    userWin++;
+    gameNumber++;
+
+}
+else {
+    gameNumber++;
+}
+
+
+console.log(pcWin);
+console.log(userWin);
